4100999248927e67d032f31691b70634c53fb3b4
Aleajda <alexus.fedorov@gmail.com>
2025-10-01 19:04:41 +0300

    docs: add readme.md for task3

--- game/src/main/java/com/example/game/configuration/CorsConfig.java
+++ game/src/main/java/com/example/game/configuration/CorsConfig.java
@@ -13,11 +13,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // разрешаем все пути
-                        .allowedOrigins("http://localhost:5175") // адрес твоего фронта
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5175")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // если используешь cookie / авторизацию
+                        .allowCredentials(true);
             }
         };
     }
--- game/src/main/java/com/example/game/exception/InvalidGameDataException.java
+++ game/src/main/java/com/example/game/exception/InvalidGameDataException.java
@@ -2,6 +2,6 @@ package com.example.game.exception;
 
 public class InvalidGameDataException extends RuntimeException {
     public InvalidGameDataException(String message) {
-        super(message   );
+        super(message);
     }
 }
